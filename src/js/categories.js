9b2b987f371ef947b216126f832bf0aa0fcc791c
Roman-Nazymko <168586482+Roman-Nazymko@users.noreply.github.com>
2025-01-24 21:50:11 +0100

    Revert "added loader"

--- src/js/categories.js
+++ src/js/categories.js
@@ -4,7 +4,6 @@ import { fetchCategories } from './api.js';
 import { getGategoriesOnPage, getResolution } from './utils.js';
 import { ShowExercisesByCategory } from './exercises.js';
 import ExerciseFilterType from './exerciseFilterType.js';
-import { showLoader, hideLoader } from './loader.js';
 
 const categoryListEl = document.querySelector('.category-list');
 const categoryContainerEl = document.querySelector('.category-container');
@@ -28,7 +27,6 @@ export const createGalleryCards = categoriesArr => {
 
 export const showCategories = async (filter, queriedPage) => {
   try {
-    showLoader();
     const response = await fetchCategories(
       filter,
       queriedPage,
@@ -48,8 +46,6 @@ export const showCategories = async (filter, queriedPage) => {
     return { page, perPage, totalPages };
   } catch (err) {
     console.log(err);
-  } finally {
-    hideLoader();
   }
 };
 
@@ -73,9 +69,7 @@ const onCategoryListElClick = event => {
   console.log(`Execute function for rendering exercises (${filter}; ${name})`);
 };
 
-const findExerciseFilterType = filter => {
+ const findExerciseFilterType = (filter) => {
   const lowerCaseFilter = filter.toLowerCase();
-  return Object.values(ExerciseFilterType).find(
-    value => value === lowerCaseFilter
-  );
+  return Object.values(ExerciseFilterType).find(value => value === lowerCaseFilter);
 };
--- src/js/loader.js
+++
@@ -1,9 +0,0 @@
-const loader = document.querySelector('.loader');
-
-export const showLoader = () => {
-  loader.classList.remove('hidden');
-};
-
-export const hideLoader = () => {
-  loader.classList.add('hidden');
-};
